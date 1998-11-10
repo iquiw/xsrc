d494b6b69123d32dc7c5a02412eb30a603abc834
cvs2svn <admin@example.com>
1998-11-10 14:13:12 +0000

    This commit was manufactured by cvs2svn to create tag 'XFree68-0524'.

--- xc/programs/xterm/version.h
+++
@@ -1,9 +0,0 @@
-/* $XFree86: xc/programs/xterm/version.h,v 3.9 1998/07/04 14:48:30 robin Exp $ */
-
-/*
- * This is the string that's printed in response to "xterm -version", or
- * embedded in "xterm -help".  It is the version of XFree86 to which this
- * version of xterm has been built.  The number in parentheses is my patch
- * number (T.Dickey).
- */
-#define XTERM_VERSION "XFree86 3.9Aj(82)"
