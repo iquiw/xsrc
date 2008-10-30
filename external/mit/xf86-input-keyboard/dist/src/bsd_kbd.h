c215559e4ea74ea86ba1b947c95b2ed7a71428c3
cvs2svn <admin@example.com>
2008-10-30 18:36:34 +0000

    This commit was manufactured by cvs2svn to create tag 'MesaDemos-7-0-3'.

--- external/mit/xf86-input-keyboard/dist/src/bsd_kbd.h
+++
@@ -1,8 +0,0 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/os-support/bsd/bsd_kbd.h,v 1.1 2002/10/11 01:40:34 dawes Exp $ */
-
-extern void KbdGetMapping(InputInfoPtr pInfo, KeySymsPtr pKeySyms,
-				CARD8 *pModMap);
-
-#ifdef __NetBSD__
-#include <dev/wscons/wsconsio.h>
-#endif
