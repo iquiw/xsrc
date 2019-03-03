8f5578a99d24765912e9a10c5c18202fdc6c0a0c
cvs2svn <admin@example.com>
2019-03-03 21:36:27 +0000

    This commit was manufactured by cvs2svn to create tag 'xinit-1-4-1'.

--- external/mit/fontconfig/dist/src/fcstdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FONTCONFIG_SRC_FCSTDINT_H
-#define _FONTCONFIG_SRC_FCSTDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "fontconfig 2.12.4"
-/* generated using gnu compiler gcc (GCC) 6.3.1 20161221 (Red Hat 6.3.1-1) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
